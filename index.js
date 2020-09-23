edd5c3a91cdf33457523bd6e1e12c29513df67d3
lesyaloveyou <71792249+lesyaloveyou@users.noreply.github.com>
2020-09-23 23:27:58 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+(function() {
+  var codeTime, commentTime, finalStyle, isOn, openComment, skip, styles, writeStyleChar, writeStyles;
+
+  styles = "/*\n * \"Любовное послание\"💕 v1.0.0\n * Авторство - Lolip1e\n */\n\nbody {\n  background-color: #1a1c24; color: #fff;\n  font-size: 1.0rem; line-height: 1.4;\n  -webkit-font-smoothing: subpixel-antialiased;\n}\n\n/*\n * ...\n * ...привет?\n * Я долго думал, какой же сделать тебе подарок\n * И в конце концов, не придумал ничего лучше\n * Чем подарить тебе вот такой вот\n * Сайтик с полным процессом создания\n * В конце получится неплохая фигурка\n * Я хоть и плохой романтик, но зато \n * Хороший программист! И надеюсь\n * Тебе понравится, Зай <3！！\n */\n\npre {\n  position: fixed;\n  top: 30px; bottom: 30px;\n  transition: left 500ms;\n  overflow: auto;\n  background-color: #313744; color: #a6c3d4;\n  border: 1px solid rgba(0,0,0,0.2);\n  padding: 24px 12px;\n  box-sizing: border-box;\n  border-radius: 3px;\n  box-shadow: 0px 4px 0px 2px rgba(0,0,0,0.1);\n}\n\n/*\n * Все что выше - это CSS, отвечающий за расположение\n * Просто решил немного поучить тебя программироованию\n * Дальше пойдет комманда, предающая цвета коду\n */\n\npre em:not(.comment) { font-style: normal; }\n\n.comment       { color: #707e84; }\n.selector      { color: #c66c75; }\n.selector .key { color: #c66c75; }\n.key           { color: #c7ccd4; }\n.value         { color: #d5927b; }\n\n\n/*\n * Потом идет фиксация позиции！\n * И перемещение в правый угол\n * Тоже с помощью CSS\n */\n\n@media screen and (max-width: 768px) {\n    pre { left: 6%;right: 6%;top: 50%; }\n}\n\n@media screen and (min-width: 768px) {\n    pre { width: 48%;left: 50%;bottom: 30px; }\n}\n\n/*\n * Кстати Лесь, я очень сильно тебя люблю <3！\n */\n\n@media screen and (max-width: 768px) {\n    #heart, #echo {\n        position: fixed;\n        width: 300px; height: 300px;\n        margin: 30px auto;\n        left: 0; right: 0;\n        text-align: center;\n        -webkit-transform: scale(0.95);\n        transform: scale(0.95);\n    }\n}\n\n@media screen and (min-width: 768px) {\n    #heart, #echo {\n      position: fixed;\n      width: 300px; height: 300px;\n      top: calc(50% - 150px); left: calc(25% - 150px);\n      text-align: center;\n      -webkit-transform: scale(0.95);\n              transform: scale(0.95);\n    }\n}\n\n#heart { z-index: 8; }\n#echo  { z-index: 7; }\n\n#heart::before, #heart::after, #echo::before, #echo::after {\n    content: '';\n    position: absolute;\n    top: 40px;\n    width: 150px; height: 240px;\n    background: #c66c75;\n    border-radius: 150px 150px 0 0;\n    -webkit-transform: rotate(-45deg);\n            transform: rotate(-45deg);\n    -webkit-transform-origin: 0 100%;\n            transform-origin: 0 100%;\n}\n\n#heart::before, #echo::before {\n  left: 150px;\n}\n\n#heart::after, #echo::after {\n  left: 0;\n  -webkit-transform: rotate(45deg);\n          transform: rotate(45deg);\n  -webkit-transform-origin: 100% 100%;\n          transform-origin: 100% 100%;\n}\n\n/* Теперь идут значения, создающие цветную область */\n\n#heart::after {\n  box-shadow:\n    inset -6px -6px 0px 6px rgba(255,255,255,0.1);\n}\n\n#heart::before {\n  box-shadow:\n    inset 6px 6px 0px 6px rgba(255,255,255,0.1);\n}\n\n/*\n * Позже ей придаётся форма\n * И прописывается положение на борде！\n * >v<\n */\n\n#heart i::before {\n  content: 'Люблю тебя, солнышко';\n  position: absolute;\n  z-index: 9;\n  width: 100%;\n  top: 35%; left: 0;\n  font-style: normal;\n  color: rgba(255,255,255,0.8);\n  font-weight: 100;\n  font-size: 30px;\n  text-shadow: -1px -1px 0px rgba(0,0,0,0.2);\n}\n\n/*\n * Теперь создается ячейка с текстом\n * Устанавливается шрифт, придается форма и тень！\n */\n\n@-webkit-keyframes heartbeat {\n  0%, 100% {\n    -webkit-transform: scale(0.95);\n            transform: scale(0.95);\n  }\n  50% {\n    -webkit-transform: scale(1.00);\n            transform: scale(1.00);\n  }\n}\n\n@keyframes heartbeat {\n  0%, 100% { transform: scale(0.95); }\n  50%      { transform: scale(1.00); }\n}\n\n@-webkit-keyframes echo {\n  0%   {\n    opacity: 0.1;\n    -webkit-transform: scale(1);\n            transform: scale(1);\n  }\n  100% {\n    opacity: 0;\n    -webkit-transform: scale(1.4);\n            transform: scale(1.4);\n  }\n}\n\n@keyframes echo {\n  0%   {\n    opacity: 0.1;\n    transform: scale(1);\n  }\n  100% {\n    opacity: 0;\n    transform: scale(1.4);\n  }\n}\n\n/*\n * Тут создается анимация, с помощью JavaScript\n */\n\n#heart, #echo {\n  -webkit-animation-duration: 2000ms;\n          animation-duration: 2000ms;\n  -webkit-animation-timing-function:\n    cubic-bezier(0, 0, 0, 1.74);\n          animation-timing-function:\n            cubic-bezier(0, 0, 0, 1.74);\n  -webkit-animation-delay: 500ms;\n          animation-delay: 500ms;\n  -webkit-animation-iteration-count: infinite;\n          animation-iteration-count: infinite;\n  -webkit-animation-play-state: paused;\n          animation-play-state: paused;\n}\n\n#heart {\n  -webkit-animation-name: heartbeat;\n          animation-name: heartbeat;\n}\n#echo {\n  -webkit-animation-name: echo;\n          animation-name: echo;\n}\n\n/*\n * Придаётся название и время воспроизведения！\n */\n\n#heart, #echo {\n\n/*\n * ...3...\n *\n * ...2...\n *\n * ...1...\n *\n * Вот как-то так это выглядит!\n *\n */\n\n  -webkit-animation-play-state: running;\n          animation-play-state: running;\n\n/*\n * ...Надеюсь!\n */\n\n}\n\n/*\n * Что тебе понравилось\n * Понимаю, не слишком оригинально\n * И вряд ли романтично >v< Но я старался\n * И убил на это не одну ночь！\n * Ведь то что ты видела, это лишь 2% от всего кода！\n * 99669999996669999996699666699666999966699666699\n * 99699999999699999999699666699669966996699666699\n * 99669999999999999996699666699699666699699666699\n * 99666699999999999966666999966699666699699666699\n * 99666666999999996666666699666699666699699666699\n * 99666666669999666666666699666669966996699666699\n * 99666666666996666666666699666666999966669999996\n * А что делать теперь？\n * Остается лишь нажать\n * Crtl+F вбить значение 99 и наслаждаться\n * Извини, но ничего более оригинального не придумал\n */\n\n/*\n * \"Любовное послание\" v1.0.0 \n * Надеюсь, тебе понравилось, любимая💕\n * Никогда не забывай, на сколько сильно я тебя люблю\n */";
+
+  finalStyle = styles;
+
+  openComment = false;
+
+  isOn = true;
+
+  writeStyleChar = function(which) {
+    if (which === '/' && openComment === false) {
+      openComment = true;
+      styles = $('#style-text').html() + which;
+    } else if (which === '/' && openComment === true) {
+      openComment = false;
+      styles = $('#style-text').html().replace(/(\/[^\/]*\*)$/, '<em class="comment">$1/</em>');
+    } else if (which === ':') {
+      styles = $('#style-text').html().replace(/([a-zA-Z- ^\n]*)$/, '<em class="key">$1</em>:');
+    } else if (which === ';') {
+      styles = $('#style-text').html().replace(/([^:]*)$/, '<em class="value">$1</em>;');
+    } else if (which === '{') {
+      styles = $('#style-text').html().replace(/(.*)$/, '<em class="selector">$1</em>{');
+    } else {
+      styles = $('#style-text').html() + which;
+    }
+    $('#style-text').html(styles);
+    return $('#style-tag').append(which);
+  };
+
+  writeStyles = function(message, index, interval) {
+    var pre;
+    if (index < message.length) {
+      pre = document.getElementById('style-text');
+      pre.scrollTop = pre.scrollHeight;
+      writeStyleChar(message[index++]);
+      if (isOn) {
+        return setTimeout((function() {
+          return writeStyles(message, index, openComment ? commentTime : codeTime);
+        }), interval);
+      }
+    }
+  };
+
+  skip = function() {
+    isOn = false;
+    return setTimeout((function() {
+      var pre;
+      $('#style-text').html(finalStyle);
+      $('#style-tag').html(finalStyle);
+      pre = document.getElementById('style-text');
+      return pre.scrollTop = pre.scrollHeight;
+    }), 2 * commentTime);
+  };
+
+  $('body').append("  <style id=\"style-tag\"></style>\n<span id=\"echo\"></span>\n<span id=\"heart\"><i></i></span>\n<pre id=\"style-text\"></pre>\n  <div style = 'position: fixed;left: 30px;bottom: 10px;'>\n    <a id='skipAnimation'>ПРОПУСТИТЬ АНИМАЦИЮ (НИНАЖИМАЙ)</a>\n    </div>");
+  commentTime = 60;
+
+  codeTime = 20;
+
+  $(document).on('ready page:load', function() {
+    return $('#skipAnimation').click(function() {
+      return skip();
+    });
+  });
+
+  writeStyles(styles, 0, commentTime);
+
+}).call(this);
